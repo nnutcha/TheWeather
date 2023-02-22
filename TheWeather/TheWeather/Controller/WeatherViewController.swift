b2cdca777904c49e3b1b4d1e675fcb51889170ac
nutnutchah <nutnutcha.h@kppfg.com>
2023-02-22 12:20:47 +0700

    Implement function to convert tempereture types

--- TheWeather/TheWeather/Controller/WeatherViewController.swift
+++ TheWeather/TheWeather/Controller/WeatherViewController.swift
@@ -12,9 +12,18 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    
+    var isFahrenheit = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    func setupView() {
+        convertButton.titleLabel?.text = "Convert to Celsius"
     }
     
     @IBAction func getCurrentWeatherButtonTapped(_ sender: UIButton) {
@@ -29,10 +38,41 @@ class WeatherViewController: UIViewController {
                     self.humidityLabel.text = "\(weather.main.humidity) %"
                     
                 }
-            case .failure(let error):
+            case .failure(_):
                 print("Error getting current weather: (error.localizedDescription)")
                 break
             }
         }
+    }
+    
+    @IBAction func convertButtonTapped(_ sender: Any) {
+        if !isFahrenheit {
+            // Convert to Fahrenheit
+            temperatureLabel.text = convertToDegreesFahrenheit(temperatureLabel.text!)
+            convertButton.setTitle("Convert to Celsius", for: .normal)
+        } else {
+            // Convert to Celsius
+            temperatureLabel.text = convertToDegreesCelsius(temperatureLabel.text!)
+            convertButton.setTitle("Convert to Fahrenheit", for: .normal)
+        }
+        isFahrenheit = !isFahrenheit
+    }
+    
+    // MARK: - Helper Methods
+    
+    func convertToDegreesCelsius(_ degreesFahrenheit: String) -> String {
+        guard let fahrenheit = Double(degreesFahrenheit) else {
+            return degreesFahrenheit
+        }
+        let celsius = (fahrenheit - 32) * 5 / 9
+        return String(format: "%.0f °C", celsius)
+    }
+    
+    func convertToDegreesFahrenheit(_ degreesCelsius: String) -> String {
+        guard let celsius = Double(degreesCelsius) else {
+            return degreesCelsius
+        }
+        let fahrenheit = celsius * 9 / 5 + 32
+        return String(format: "%.0f °F", fahrenheit)
     }
 }

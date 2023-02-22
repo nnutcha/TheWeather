52d998e068752ef00cb0bebe2cb87d4cb7ffd4c2
nutnutchah <nutnutcha.h@kppfg.com>
2023-02-22 09:58:01 +0700

    First commit - create xcode project

---
+++ TheWeather/TheWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheWeather
+//
+//  Created by Nutnutcha Hanggitthagul on 22/2/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

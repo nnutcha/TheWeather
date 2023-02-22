ea98fa94725e6450008d362413fc38af315fcef2
nutnutchah <nutnutcha.h@kppfg.com>
2023-02-22 13:16:51 +0700

    Implement weather image

--- TheWeather/TheWeather/Controller/WeatherViewController.swift
+++ TheWeather/TheWeather/Controller/WeatherViewController.swift
@@ -11,19 +11,24 @@ class WeatherViewController: UIViewController {
     
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var temperatureTypeLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var weatherImageView: UIImageView!
     
     var isFahrenheit = true
-    
+    var weatherCode: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+     
         setupView()
     }
     
     func setupView() {
-        convertButton.titleLabel?.text = "Convert to Celsius"
+        if isFahrenheit == true {
+            temperatureTypeLabel.text = "°F (Fahrenheit)"
+        }
     }
     
     @IBAction func getCurrentWeatherButtonTapped(_ sender: UIButton) {
@@ -33,10 +38,14 @@ class WeatherViewController: UIViewController {
         NetworkManager.getCurrentWeather(for: city) { result in
             switch result {
             case .success(let weather):
-                DispatchQueue.main.async {
-                    self.temperatureLabel.text = "\(weather.main.temp) °F"
+                DispatchQueue.main.async { [self] in
+                    self.temperatureLabel.text = "\(weather.main.temp)"
                     self.humidityLabel.text = "\(weather.main.humidity) %"
+                    self.weatherCode = weather.weather.first?.id ?? 0
                     
+                    let weatherCondition = WeatherCondition(code: self.weatherCode)
+                    let imageName = weatherCondition.imageName
+                    self.weatherImageView.image = UIImage(named: imageName)
                 }
             case .failure(_):
                 print("Error getting current weather: (error.localizedDescription)")
@@ -49,10 +58,12 @@ class WeatherViewController: UIViewController {
         if !isFahrenheit {
             // Convert to Fahrenheit
             temperatureLabel.text = convertToDegreesFahrenheit(temperatureLabel.text!)
+            temperatureTypeLabel.text = "°F (Fahrenheit)"
             convertButton.setTitle("Convert to Celsius", for: .normal)
         } else {
             // Convert to Celsius
             temperatureLabel.text = convertToDegreesCelsius(temperatureLabel.text!)
+            temperatureTypeLabel.text = "°C (Celsius)"
             convertButton.setTitle("Convert to Fahrenheit", for: .normal)
         }
         isFahrenheit = !isFahrenheit
@@ -65,7 +76,7 @@ class WeatherViewController: UIViewController {
             return degreesFahrenheit
         }
         let celsius = (fahrenheit - 32) * 5 / 9
-        return String(format: "%.0f °C", celsius)
+        return String(format: "%.0f", celsius)
     }
     
     func convertToDegreesFahrenheit(_ degreesCelsius: String) -> String {
@@ -73,6 +84,6 @@ class WeatherViewController: UIViewController {
             return degreesCelsius
         }
         let fahrenheit = celsius * 9 / 5 + 32
-        return String(format: "%.0f °F", fahrenheit)
+        return String(format: "%.0f", fahrenheit)
     }
 }
